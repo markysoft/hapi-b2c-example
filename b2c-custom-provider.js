b10260f8ab7a171080e12d0303e5ebe6c547622b
Mark Harrop <mark.harrop@defra.gov.uk>
2020-05-01 12:05:20 +0100

    initial work

---
+++ b2c-custom-provider.js
@@ -0,0 +1,26 @@
+function b2cCustomProvider (options) {
+  console.log(`openid+${options.clientId}`)
+  return {
+    protocol: 'oauth2',
+    useParamsAuth: true,
+    auth: `${options.uri}/oauth2/v2.0/authorize`,
+    token: `${options.uri}/oauth2/v2.0/token`,
+    scope: [`openid+${options.clientId}`],
+    profile: async function (credentials, params, get) {
+      // this should be replaced with a call to get user info
+      const profile = await Promise.resolve({ email: 'test@mail', given_name: 'test', family_name: 'user' })
+
+      credentials.profile = {
+        id: profile.sub,
+        username: profile.email,
+        displayName: profile.nickname,
+        firstName: profile.given_name,
+        lastName: profile.family_name,
+        email: profile.email,
+        raw: profile
+      }
+    }
+  }
+}
+
+module.exports = b2cCustomProvider
---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict'
+require('dotenv').config()
+
+const Bell = require('@hapi/bell')
+Bell.providers['b2c-custom'] = require('./b2c-custom-provider')
+const Boom = require('@hapi/boom')
+const Hapi = require('@hapi/hapi')
+
+// Build config
+const b2cConfig = {
+  uri: process.env.B2C_URI,
+  clientId: process.env.B2C_CLIENT_ID,
+  clientSecret: process.env.B2C_CLIENT_SECRET
+}
+const internals = {}
+
+internals.start = async function () {
+  const server = Hapi.server({ port: 3006 })
+  await server.register([require('@hapi/cookie'), Bell])
+
+  server.auth.strategy('session', 'cookie', {
+    cookie: {
+      name: 'sid-demo',
+      path: '/',
+      password: 'cookie_encryption_password_secure',
+      isSecure: false // Should be set to true (which is the default) in production
+    },
+    redirectTo: '/auth/b2c' // If there is no session, redirect here
+  })
+
+  server.auth.strategy('b2c', 'bell', {
+    provider: 'b2c-custom',
+    config: {
+      uri: b2cConfig.uri,
+      clientId: b2cConfig.clientId
+    },
+    providerParams: {
+      p: 'b2c_1a_ff_demo_token_scopeaslist',
+      response_type: 'token+id_token',
+      nonce: 'defaultNonce',
+      prompt: 'login'
+    },
+    password: 'cookie_encryption_password_secure',
+    isSecure: false,
+    scope: ['email', 'profile', 'openid'],
+    location: 'http://localhost:3006',
+    clientId: b2cConfig.clientId,
+    clientSecret: b2cConfig.clientSecret
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/auth/b2c',
+    options: {
+      auth: {
+        strategy: 'b2c',
+        mode: 'try'
+      },
+      handler: function (request, h) {
+        console.log('/auth/b2c')
+        if (!request.auth.isAuthenticated) {
+          throw Boom.unauthorized('Authentication failed: ' + request.auth.error.message)
+        }
+
+        console.log('authenticated', request.auth.credentials.profile.username)
+        // Just store the third party credentials in the session as an example. You could do something
+        // more useful here - like loading or setting up an account (social signup).
+
+        request.cookieAuth.set(request.auth.credentials)
+        return h.redirect('/')
+      }
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    options: {
+      auth: 'session',
+      handler: function (request, h) {
+        // Return a message using the information from the session
+
+        return 'Hello, ' + request.auth.credentials.profile.username + '!'
+      }
+    }
+  })
+
+  await server.start()
+  console.log('Server started at:', server.info.uri)
+}
+
+internals.start()
